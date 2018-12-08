ef79a6dd56f3b5ee4113ccf9a00a6cfcadcb7f01
tmlima <thiago.machado.lima@gmail.com>
2018-12-08 17:08:29 -0200

    Counting texts with more indirect relations and with more chains containing at least one indirect relation

--- SummitRelations/SummitRelations/Program.cs
+++ SummitRelations/SummitRelations/Program.cs
@@ -7,6 +7,8 @@ namespace SummitRelations
 {
     class Program
     {
+        const string Indirect = "indirect";
+
         static void Main(string[] args)
         {
             string textsFolder = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "/Texts";
@@ -24,6 +26,9 @@ namespace SummitRelations
 
             NonDirectMarkables(markables);
 
+            TextsMoreIndirectRelations( markables );
+            TextsWithMoreChainsContainingAtLeatsOneIndirect( markables );
+
             Console.ReadKey();
             Console.ReadLine();
         }
@@ -31,7 +36,8 @@ namespace SummitRelations
         //elementos da cadeia que a qual ele pertence que vieram antes dele e que não sejam direto ordernados pela primeira palavra do span
         static void ElementosDaCadeiaAnterioresIndiretos(List<Pos> pos, List<Markable> markables)
         {
-            List<Markable> indirects = markables.Where(x => x.IsAnaphoric == "indirect").ToList();
+            List<Markable> indirects = markables.Where(x => x.
+            IsAnaphoric == Indirect ).ToList();
 
             markables = markables.OrderBy(x => x.Text).ToList();
             List<string> texts = markables.Select(x => x.Text).Distinct().ToList();
@@ -46,7 +52,7 @@ namespace SummitRelations
                     {
                         Console.WriteLine("Set " + set);
                         List<Markable> setMarkables = textMarkables.Where(x => x.Member == set).OrderBy(x => x.FirstWordIndex).ToList();
-                        int lastIndirectIndex = setMarkables.IndexOf(setMarkables.Last(x => x.IsAnaphoric == "indirect"));
+                        int lastIndirectIndex = setMarkables.IndexOf(setMarkables.Last(x => x.IsAnaphoric == Indirect));
                         for (int i=0; i<=lastIndirectIndex; i++)
                         {
                             Markable markable = setMarkables[i];
@@ -64,7 +70,7 @@ namespace SummitRelations
         //quantos dos 331 indiretos são plural (que tem pelo menos um plural)
         static void IndiretosPlural(List<Pos> pos, List<Markable> markables)
         {
-            List<Markable> indirects = markables.Where(x => x.IsAnaphoric == "indirect").ToList();
+            List<Markable> indirects = markables.Where(x => x.IsAnaphoric == Indirect).ToList();
             int found = 0;
             foreach (Markable m in indirects)
             {
@@ -89,6 +95,67 @@ namespace SummitRelations
             {
                 Console.WriteLine("[" + t + "]: " + markables.Where(x => x.IsAnaphoric == t).Count());
             }
+        }
+
+        static void TextsMoreIndirectRelations(List<Markable> markables)
+        {
+            Dictionary<string, int> textIndirectRelationsDictionary = new Dictionary<string, int>();
+
+            foreach (Markable m in markables)
+            {
+                if (m.IsAnaphoric == Indirect )
+                {
+                    if ( textIndirectRelationsDictionary.ContainsKey( m.Text ) )
+                        textIndirectRelationsDictionary[ m.Text ] += 1;
+                    else
+                        textIndirectRelationsDictionary.Add( m.Text, 1 );
+                }
+            }
+
+            List<KeyValuePair<string, int>> textIndirectRelations = textIndirectRelationsDictionary
+                .ToList()
+                .OrderByDescending( x => x.Value )
+                .ToList();
+
+            Console.WriteLine( "3 texts with more indirect relations:" );
+            foreach (KeyValuePair<string, int> k in textIndirectRelations.Take( 3 ))
+                Console.WriteLine( "Text [" + k.Key + "] : " + k.Value );
+        }
+
+        static void TextsWithMoreChainsContainingAtLeatsOneIndirect(List<Markable> markables)
+        {
+            Dictionary<string, int> textChainsWithAtLeastOneIndirectDictionary = new Dictionary<string, int>();
+
+            List<string> texts = markables.Select( x => x.Text ).Distinct().ToList();
+            foreach (string t in texts)
+            {
+                textChainsWithAtLeastOneIndirectDictionary.Add( t, ChainsWithAtLeastOneIndirect( t, markables ) );
+            }
+
+            List<KeyValuePair<string, int>> textChainsWithAtLeastOneIndirect = textChainsWithAtLeastOneIndirectDictionary
+                .ToList()
+                .OrderByDescending( x => x.Value )
+                .ToList();
+
+            Console.WriteLine( "3 texts with more chains containing indirect relations:" );
+            foreach ( KeyValuePair<string, int> k in textChainsWithAtLeastOneIndirect.Take( 3 ) )
+                Console.WriteLine( "Text [" + k.Key + "] : " + k.Value );
+        }
+
+        static int ChainsWithAtLeastOneIndirect(string text, List<Markable> markables)
+        {
+            int chainsWithAtLeaseOneIndirect = 0;
+            List<string> textChains = markables
+                .Where( x => x.Text == text )
+                .Select( x => x.Member )
+                .Distinct()
+                .ToList();
+
+            foreach ( string c in textChains )
+                if ( markables.Any( x => x.Member == c && x.IsAnaphoric == Indirect ) )
+                    chainsWithAtLeaseOneIndirect++;
+
+            return chainsWithAtLeaseOneIndirect;
         }
     }
 }
